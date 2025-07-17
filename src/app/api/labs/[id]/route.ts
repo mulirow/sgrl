809a57a70560a47992dd51fcf11ca1eadf058627
Felipe Oliveira <felipesansoliv01@gmail.com>
2025-07-17 20:46:30 -0300

    feat(backend): adding delete and update routes for labs

---
+++ src/app/api/labs/[id]/route.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = Number(params.id);
+  const data = await req.json();
+
+  try {
+    const updatedLab = await prisma.laboratorio.update({
+      where: { id: params.id },
+      data,
+    });
+
+    return NextResponse.json(updatedLab, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Lab not found or update failed." },
+      { status: 404 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+
+  try {
+    await prisma.laboratorio.delete({
+      where: { id },
+    });
+
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Lab not found or delete failed." },
+      { status: 404 }
+    );
+  }
+}
