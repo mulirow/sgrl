457317a063323cc323358b75d8905b2412934f4b
Felipe Oliveira <felipesansoliv01@gmail.com>
2025-07-17 21:16:24 -0300

    fix: TypeError: does not satisft the constraint 'ParamCheck<RouteContext>'

--- src/app/api/labs/[id]/route.ts
+++ src/app/api/labs/[id]/route.ts
@@ -1,46 +1,47 @@
-import { PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export async function PATCH(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const data = await req.json();
-
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+): Promise<NextResponse> {
   try {
+    const id = (await params).id;
+    const data = await request.json();
+
     const updatedLab = await prisma.laboratorio.update({
-      where: { id: params.id },
+      where: { id },
       data,
     });
 
-    return NextResponse.json(updatedLab, { status: 200 });
+    return NextResponse.json(updatedLab);
   } catch (error) {
-    console.error("Error updating lab:", error);
+    console.error('Error updating lab:', error);
     return NextResponse.json(
-      { error: "Lab not found or update failed." },
+      { error: 'Lab not found or update failed' },
       { status: 404 }
     );
   }
 }
 
 export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const id = params.id;
-
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+): Promise<NextResponse> {
   try {
+    const id = (await params).id;
+
     await prisma.laboratorio.delete({
       where: { id },
     });
 
-    return new Response(null, { status: 204 });
+    return new NextResponse(null, { status: 204 });
   } catch (error) {
-    console.error("Error deleting lab:", error);
+    console.error('Error deleting lab:', error);
     return NextResponse.json(
-      { error: "Lab not found or delete failed." },
+      { error: 'Lab not found or delete failed' },
       { status: 404 }
     );
   }
