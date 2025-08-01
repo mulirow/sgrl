107c14d3d403099cc0238aab36c863c35fc87ee7
Murilo Barbosa <mbn2@cin.ufpe.br>
2025-08-01 00:09:42 -0300

    feat: export default prisma client to integrate with @/auth.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    });
 
 if (process.env.NODE_ENV !== 'production') {
-    global.prisma = prisma;
+    globalForPrisma.prisma = prisma;
 }
+
+export default prisma;
