be9c9ecaaa3ac8b4e406738fc250a08dafd5599c
Murilo Barbosa <mbn2@cin.ufpe.br>
2025-08-06 23:21:41 -0300

    fix: hydration failed because the server rendered HTML didn't match the client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html lang="pt-BR" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
